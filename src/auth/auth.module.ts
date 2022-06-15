81bfed1e02c2b78de90c94eb1d768835bf0b5425
Zechariah-dev <omoladesunday2017@gmail.com>
2022-06-15 12:32:30 +0100

    setup passport-jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,19 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthHelper } from './auth.helper';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [JwtModule.register({ secret: 'hardguess' })],
+  imports: [
+    JwtModule.register({
+      secret: 'hardguess',
+      signOptions: {
+        expiresIn: '2h',
+      },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, AuthHelper],
+  providers: [AuthService, AuthHelper, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
